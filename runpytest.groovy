a6feeb89772e67255e2bbdb02db2765c22652396
jiusheng_kuo <jiusheng_kuo@compalbn.com>
2024-02-28 19:50:29 +0800

    Added gitignore, readme, jenkins pipeline example
    Change-Id: I9f5c8fb2657b82ddc06a3f45c03a9bef13eb04d0

---
+++ runpytest.groovy
@@ -0,0 +1,39 @@
+pipeline {
+    agent {
+        label 'macos'
+    }
+    stages {
+        stage('Checkout') {
+            steps {
+                git 'https://github.com/love1357983/pytest_demo.git'
+            }
+        }
+        
+        stage('Initialize Virtual Environment') {
+            steps {
+                sh '''python3 -m venv venv
+                source ./venv/bin/activate
+                pip3 install -r requirements.txt
+                '''
+            }
+        }
+
+        stage('Run Tests') {
+            steps {
+                sh '''source ./venv/bin/activate
+                pytest --alluredir=allure-results
+                '''
+            }
+        }
+        
+        stage('Publish Allure Report') {
+            steps {
+                allure([
+                    includeProperties: false,
+                    jdk: '',
+                    results: [[path: 'allure-results']]
+                ])
+            }
+        }
+    }
+}
