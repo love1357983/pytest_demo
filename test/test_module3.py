189875ef14c8fa24612379c1d9008f237bf2c59a
jiusheng <jiusheng_kuo@compalbn.com>
2024-03-08 18:05:34 +0800

    modify amo test case.
    Change-Id: Ia8ad9a133188def1e86b9e4b184c8c3a86ee99ff

--- test/test_module3.py
+++ test/test_module3.py
@@ -5,19 +5,21 @@ from libs.module import Module
 @allure.epic("DOCSIS")
 class TestDOCSIS_getTURE:
     @allure.feature("DOCSIS_getTURE")
-    @allure.title("amo say get TURE")
-    @allure.testcase("NTLSAN-096", "TC amo say get TURE")
+    @allure.title("Verify amo say get TURE")
+    @allure.testcase(https://jira.compalbn.com, "NTLSAN-096")
     @allure.severity(allure.severity_level.CRITICAL)
     def test_basic1_get_true_func():
         with allure.step("1. verify values1"):
-            assert Module.get_true() is False
+            assert Module.get_true() is True
+        with allure.step("2. verify values2"):
+            assert Module.get_true() is True
 
 @allure.epic("DOCSIS")
 class TestDOCSIS_getFALSE:
     @allure.feature("DOCSIS_getFALSE")
-    @allure.title("amo say get FALSE")
-    @allure.testcase("NTLSAN-097", "TC amo say get FALSE")
+    @allure.title("Verfiy amo say get FALSE")
+    @allure.testcase(https://jira.compalbn.com, "NTLSAN-097")
     @allure.severity(allure.severity_level.MINOR)
     def test_basic1_get_false_func():
         with allure.step("1. verify values2"):
-            assert Module.get_false() is True
+            assert Module.get_false() is False
