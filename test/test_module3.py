7c5f36daa4cc4c401e297c48fd5f14429ee7e5ac
jiusheng <jiusheng_kuo@compalbn.com>
2024-03-08 20:19:54 +0800

    modify requirements for python 3.6.9 and fixed class function by self
    Change-Id: Ibb69cc25da34bd47cd818a503b53b2f147432877

--- test/test_module3.py
+++ test/test_module3.py
@@ -8,7 +8,7 @@ class TestDOCSIS_getTURE:
     @allure.title("Verify amo say get TURE")
     @allure.testcase("https://jira.compalbn.com", "NTLSAN-096")
     @allure.severity(allure.severity_level.CRITICAL)
-    def test_basic1_get_true_func():
+    def test_basic1_get_true_func(self):
         with allure.step("1. verify values1"):
             assert Module.get_true() is True
         with allure.step("2. verify values2"):
@@ -20,6 +20,6 @@ class TestDOCSIS_getFALSE:
     @allure.title("Verfiy amo say get FALSE")
     @allure.testcase("https://jira.compalbn.com", "NTLSAN-097")
     @allure.severity(allure.severity_level.MINOR)
-    def test_basic1_get_false_func():
+    def test_basic1_get_false_func(self):
         with allure.step("1. verify values2"):
             assert Module.get_false() is False
